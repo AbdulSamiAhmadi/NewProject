6537fee9d9451b9456ae047f00c1ba00820d77bc
samiahmadi <samiahmadi@localhost>
2023-07-29 12:00:18 -0400

    New Project

---
+++ src/newproject/NewProject.java
@@ -0,0 +1,19 @@
+package newproject;
+
+public class NewProject {
+
+	public static void main(String[] args) {
+		
+		String name = "Ahmadi";
+		int id = 200;
+		String state = "VA";
+		String country = "USA";
+		
+		System.out.println(name);
+		System.out.println(id);
+		System.out.println(state);
+		System.out.println(country);
+		
+	}
+
+}
